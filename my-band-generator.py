6aad45e2d13d88f9888f81092ce8d76ad28cb234
Oleksandra Protsenko <64093597+ProtsenkoOleksandra@users.noreply.github.com>
2025-01-22 10:42:03 +0100

    Add files via upload
    Band Name Generator Game
    This fun and interactive game allows you to create your own band name! Simply follow the prompts, and the generator will come up with a unique name based on your inputs. The game is in Spanish, so it's a great opportunity to practice the language while having fun! Enjoy and let your creativity flow!

---
+++ my-band-generator.py
@@ -0,0 +1,4 @@
+print("¡Hola! Escuché que quieres formar tu propia banda musical y no sabes cómo nombrarla. Este generador de nombres te ayudará. ¡Vamos!")
+a = input("Escribe el nombre de la ciudad en la que naciste:\n")
+b = input("Escribe un animal (con la primera letra en mayúscula):\n")
+print("¡El nombre de tu banda será: " + b + " de " + a + "! ¡Qué épico!")
